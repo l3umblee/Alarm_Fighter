0865be4ebaa89a6bd9103403c4ca3b83d6d28dc7
capableofanything <sunho990712@naver.com>
2023-02-08 18:25:10 +0900

    RoundField and prefab

---
+++ Assets/Scripts/Field/RoundField.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundField : MonoBehaviour
+{
+    protected GameObject Grid_All;
+
+    [SerializeField]
+    int height ;
+    [SerializeField]
+    int width ;
+
+    List<List<Imfor>> gridArray = new List<List<Imfor>>();// 2 demention list
+
+    private void FindObject()
+    {
+        Grid_All = gameObject;
+    }
+    private void Init()
+    {
+        FindObject();
+
+        for(int i = 0; i < width; i++)
+            gridArray.Add(new List<Imfor>());
+
+        int index = 0;
+        for(int i=0; i < width; i++)
+        {
+            for(int j=0;j<height;j++)
+            {
+                Imfor imfor = new Imfor();
+                imfor.grid = transform.GetChild(index).gameObject;
+                gridArray[i].Add(imfor);
+                index++;
+            }
+        }
+        
+    }
+    void Awake()    
+    {
+        Init();
+    }
+}
+
+class Imfor
+{
+    public GameObject grid; // one prefab
+    public float ratio = 1.0f; // used by player scale
+}
+
